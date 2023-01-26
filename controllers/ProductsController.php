670c755864a2db06f32e74850d3c2c294aa7a7e5
Kevinr2 <kevinreve096@gmail.com>
2023-01-26 08:26:18 -0500

    controller listar y crear

--- controllers/ProductsController.php
+++ controllers/ProductsController.php
@@ -8,4 +8,28 @@ class ProductsController
         $product = Product::all();
         var_dump($product);
     }
+    public function find()
+    {
+        $product = Product::findOne(2);
+        var_dump($product);
+    }
+    public function create()
+    {
+        $product = new Product();
+        $product->nombre_producto = "agua";
+        $product->referencia = "16b3f6";
+        $product->precio = "2500";
+        $product->peso = "2";
+        $product->categoria = "bebida";
+        $product->stock = "200";
+        $product->fecha_creacion = "2023-01-08";
+        $product->create();
+    }
+
+    public function update()
+    {
+    }
+    public function delete()
+    {
+    }
 }
--- models/Product.php
+++ models/Product.php
@@ -2,12 +2,39 @@
 
 class Product extends DB
 {
+    public $id;
+    public $nombre_producto;
+    public $referencia;
+    public $precio;
+    public $peso;
+    public $categoria;
+    public $stock;
+    public $fecha_creacion;
+
     public static function all()
     {
         $db = new DB();
         $prepare = $db->prepare("SELECT * From inventario");
         $prepare->execute();
-
         return $prepare->fetchAll(PDO::FETCH_CLASS, Product::class);
+    }
+    public static function findOne($id)
+    {
+        $db = new DB();
+        $prepare = $db->prepare("SELECT * FROM inventario WHERE id = :id");
+        $prepare->execute([":id" => $id]);
+
+        return $prepare->fetchObject(Product::class);
+    }
+    public  function create()
+    {
+        $prepare = $this->prepare("INSERT INTO 
+        inventario(nombre_producto,referencia,precio,peso,categoria,stock,fecha_creacion)
+        VALUES (:nombre_producto,:referencia,:precio,:peso,:categoria,:stock,:fecha_creacion)");
+        $prepare->execute([
+            ":nombre_producto" => $this->nombre_producto, ":referencia" => $this->referencia,
+            ":precio" => $this->precio, ":peso" => $this->peso, ":categoria" => $this->categoria, ":stock" => $this->stock,
+            ":fecha_creacion" => $this->fecha_creacion
+        ]);
     }
 }
